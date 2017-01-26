045314db09bf06b626b495a2d1bcabd2c93bfa92
chrissng <chris@data.gov.sg>
2017-01-26 16:35:49 +0800

    Dockerfile for testing should use python-spark also

--- tests/test.sh
+++ tests/test.sh
@@ -10,3 +10,7 @@ py.test tests/
 set +e
 echo "Running python linting"
 pylint --rcfile=tests/.pylintrc -f parseable `find dags -iname "*.py" | grep -v "node_modules/"` | tee tmp/pylint.txt
+
+pushd $SPARK_HOME
+./bin/spark-submit examples/src/main/python/pi.py 1000
+popd
