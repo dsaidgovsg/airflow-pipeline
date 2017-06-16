85e7e4ece0d3a02e81a3c31922b137e95eda199a
Chris Sng <chrissng@users.noreply.github.com>
2017-06-16 08:43:36 +0800

    Remove auto backfill

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,14 +38,8 @@ gosu "${USER}" sed -i "/\(^max_threads = \).*/ s//\1${AIRFLOW__SCHEDULER__MAX_TH
 gosu "${USER}" airflow initdb # https://groups.google.com/forum/#!topic/airbnb_airflow/4ZGWUzKkBbw
 
 if [ "$1" = 'afp-scheduler' ]; then
-    gosu "${USER}" airflow list_dags | (grep '^fn_' || echo -n "") | while read fn_dag; do
-        echo "Back filling DAG ${fn_dag}"
-        gosu "${USER}" airflow backfill -s $(date --date="yesterday-10 days" +%Y-%m-%d) -e $(date --date="yesterday" +%Y-%m-%d) -m ${fn_dag}
-    done
-    # gosu "${USER}" airflow backfill -s $(date --date="yesterday-10 days" +%Y-%m-%d) -e $(date --date="yesterday" +%Y-%m-%d) -m $(airflow list_dags | tail -n1)
-
-    (while :; do echo 'Serving logs'; gosu "${USER}" airflow serve_logs; sleep 1; done) &
-    (while :; do echo 'Starting scheduler'; gosu "${USER}" airflow scheduler -n ${SCHEDULER_RUNS}; sleep 1; done)
+  (while :; do echo 'Serving logs'; gosu "${USER}" airflow serve_logs; sleep 1; done) &
+  (while :; do echo 'Starting scheduler'; gosu "${USER}" airflow scheduler -n ${SCHEDULER_RUNS}; sleep 1; done)
 elif [ "$1" = 'afp-webserver' ]; then
   echo "Starting webserver"
   python "${AIRFLOW_HOME}"/setup_auth.py
